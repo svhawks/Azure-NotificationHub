c9688fd82f0130113fb97febdb557a08e0e16ab5
melihmucuk <melihmucuk@gmail.com>
2015-01-26 10:31:35 +0200

    push template fixed

--- AzureNotificationHub/NotificationTemplate.cs
+++ AzureNotificationHub/NotificationTemplate.cs
@@ -10,21 +10,19 @@ namespace AzureNotificationHub
     {
         public string GetPushTemplate(string alert, string badge, Dictionary<string, string> data)
         {
-            JObject pushRoot = new JObject();
-            JObject pushBody = new JObject();
-            List<JProperty> optionalFields = new List<JProperty>();
+            var pushRoot = new JObject();
+            var pushBody = new JObject();
+            var optionalFields = new List<JProperty>();
 
             if (!String.IsNullOrEmpty(alert))
                 pushBody.Add(new JProperty("alert", alert));
             if (!String.IsNullOrEmpty(badge))
                 pushBody.Add(new JProperty("badge", badge));
-            if (data != null)
-            {
-                optionalFields.AddRange(data.Select(item => new JProperty(item.Key, item.Value)));
-                pushBody.Add(optionalFields);
-            }
 
             pushRoot.Add("aps", pushBody);
+            if (data == null) return pushRoot.ToString(Formatting.None);
+            optionalFields.AddRange(data.Select(item => new JProperty(item.Key, item.Value)));
+            pushRoot.Add(optionalFields);
             return pushRoot.ToString(Formatting.None);
         }
     }
